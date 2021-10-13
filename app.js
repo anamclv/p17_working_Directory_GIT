7d17ad0700453b485de85d4b6d5e4ec9ed0325df
cursomongo <ana3.mc@gmail.com>
2021-10-13 20:28:03 +0200

    add port const

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     //codigo 
 })
 
-app.listen(3000, () =>{
-    console.log('servidor escuchando en http://localhost:3000');
+app.listen(port, () =>{
+    console.log(`servidor escuchando en http://localhost:${port}`);
 })

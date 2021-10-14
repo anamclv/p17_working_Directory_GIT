dd6c000627217a134fa4d7c4be309b0a2d70b91a
cursomongo <ana3.mc@gmail.com>
2021-10-14 20:19:18 +0200

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const Customer = require('./models/customer');
+const Log = require('./middleware/log');
+const cors = require('cors');
+
+app.use(cors());
 
 app.post('/', () => {
     Customer.getCustomer();
---
+++ models/customer.js
@@ -0,0 +1,11 @@
+const Customer = () =>{
+    return{ name: String, cif: String}
+    }
+
+    const setCustomer = (customer) =>{
+        //
+        }
+    
+   
+
+module.exports = {getCustomer, Customer};
